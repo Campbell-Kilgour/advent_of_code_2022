d38a58eb6b94b82f08df36d20c7213799d604cff
Campbell-Kilgour <65951539+Campbell-Kilgour@users.noreply.github.com>
2023-11-21 19:01:43 +0800

    Initial commit

---
+++ day2/target/debug/deps/day2-5f4825f01f80c6bc.d
@@ -0,0 +1,5 @@
+/Users/campbellkilgour/Documents/GitHub/advent_of_code_2022/day2/target/debug/deps/day2-5f4825f01f80c6bc.rmeta: src/main.rs
+
+/Users/campbellkilgour/Documents/GitHub/advent_of_code_2022/day2/target/debug/deps/day2-5f4825f01f80c6bc.d: src/main.rs
+
+src/main.rs:
---
+++ day2/target/debug/deps/day2-8be194308e0486d4.d
@@ -0,0 +1,5 @@
+/Users/campbellkilgour/Documents/GitHub/advent_of_code_2022/day2/target/debug/deps/day2-8be194308e0486d4.rmeta: src/main.rs
+
+/Users/campbellkilgour/Documents/GitHub/advent_of_code_2022/day2/target/debug/deps/day2-8be194308e0486d4.d: src/main.rs
+
+src/main.rs:
